dbe91e717882f1db7f6cc5e65ad7d03515bffe71
Joe Cortopassi <joe@joecortopassi.com>
2017-12-20 09:49:19 -0600

    added note

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
+// Kidz search is tied to a google custom search hidden behind an api,
+// what about just using simple.wikipedia and a white list of terms.
+// Would need a quick/easy way to add new terms for when they think of
+// something new they want to read about
+//
+// Might also be good to remove the typing aspect all together, and
+// just make it a visual search that they can scroll through. They
+// can drill down topically, then have a grid of buttons for each
+// subject to read about
 const url = "https://search.kidzsearch.com/kzwikisearch.php?q=whale%20shark";
 axios
   .get(url)
